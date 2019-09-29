038d9c3c819291d68373c6175673834b8f014b0e
Vincetz <55618999+Vincetz@users.noreply.github.com>
2019-09-29 18:05:07 +0300

    Update hw_2.py

--- Lesson02/hw_2.py
+++ Lesson02/hw_2.py
@@ -1,13 +1,13 @@
 thislist = [float(x) for x in input("Введите 2 числа: ").split()]
 thislist.sort()
-if thislist[1] < 0:
-    thislist2 = list(range(int(thislist[0] // 1 + 1), int((thislist[1]))))
-    print(thislist2)
-elif thislist[1] % 1 > 0:
-    thislist2 = list(range(int(thislist[0] // 1 + 1), int((thislist[1]+1))))
-    print(thislist2)
-else:
-    thislist2 = list(range(int(thislist[0]//1 + 1), int((thislist[1]))))
-    print(thislist2)
+if thislist[-1] % 1 > 0:
+    thislist[-1] = thislist[-1]+1
+thislist2 = list(range(int(thislist[0]), int(thislist[1])))
+pos = []
+for x in thislist2:
+    if x > 0:
+        pos.append(x)
+        continue
+print(pos)
     
     #Код написан в кооперации с Варбанцом
