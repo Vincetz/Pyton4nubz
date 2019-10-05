256acb3d1b2e218bb4baad23cc53af95f5a73cbc
Vincetz <55618999+Vincetz@users.noreply.github.com>
2019-10-05 17:41:32 +0300

    Create hw_2.py

---
+++ Lesson03/hw_2.py
@@ -0,0 +1,29 @@
+def nok(a, b):
+    m = a * b
+    while a != 0 and b != 0:
+        if a > b:
+            a %= b
+        else:
+            b %= a
+    return m // (a+b)
+
+if name == 'main':
+    while True:
+        try:
+            x, y = [int(x) for x in input("Укажите 2 числа: ").split()]
+            if x < 0 and y < 0:
+                x *= -1
+                y *= -1
+                print('НОК этих чисел:', nok(x, y))
+            elif x < 0:
+                x *= -1
+                print('НОК этих чисел:', nok(x, y))
+            elif y < 0:
+                y *= -1
+                print('НОК этих чисел:', nok(x, y))
+            else:
+                print('НОК этих чисел:', nok(x, y))
+        except ValueError:
+            print("Введение дробных или символьных значений не возможно!\n"
+                  "Введите 2 целых числа через пробел!")
+            continue
