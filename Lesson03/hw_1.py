254e9c35fe71e47e8229a98d61bebd4cf3af0a0f
Vincetz <55618999+Vincetz@users.noreply.github.com>
2019-10-05 17:29:29 +0300

    Create hw_1.py

---
+++ Lesson03/hw_1.py
@@ -0,0 +1,18 @@
+def calc(n):
+    n = abs(n)
+    res = 0
+    while n > 0:
+        n //= 10
+        res += 1
+    return res
+
+
+if name == 'main':
+print('Калькулятор длинны целого числа.')
+while True:
+    try:
+        n = int(input('Введите число: '))
+        print(calc(n))
+    except ValueError:
+        print("Вы ввели не целое число, попробуйте еще раз!")
+        continue
