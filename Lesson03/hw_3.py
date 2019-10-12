ddbaaa7e72e001bfe6284a7db6c35178d3d17934
Vincetz <55618999+Vincetz@users.noreply.github.com>
2019-10-12 22:36:38 +0300

    Update hw_3.py

--- Lesson03/hw_3.py
+++ Lesson03/hw_3.py
@@ -1,81 +1,59 @@
-def digit_count(fulltext):
-    num = [int(i) for i in fulltext if i.isdigit()]
-    digitdict = {'Количество цифр в тексте': len(num)}
-    return digitdict
+def rect(a, b):
+    if a < 0 and b < 0:
+        a *= -1
+        b *= -1
+    elif a < 0:
+        a *= -1
+    elif b < 0:
+        b *= -1
+    return a * b
 
 
-def word_count(text6, thisnumberlist2):
-    thisworddict = dict(zip(text6, thisnumberlist2))
-    return thisworddict
+def triangle(a, b, c):
+    if a < 0 and b < 0 and c < 0:
+        a *= -1
+        b *= -1
+        c *= -1
+    elif a < 0:
+        a *= -1
+    elif b < 0:
+        b *= -1
+    elif c < 0:
+        c *= -1
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
 
-def char_count(text3, thisnumberlist ):
-    thischardict = dict(zip(text3, thisnumberlist))
-    return thischardict
-
-
-def text_stat(text, *args):
-    textargs = ""
-    textargs += "%s" % args
-    fulltext = text + "" + textargs
-    fulltext = fulltext.casefold()
-    linecount = {"Количество строк": fulltext.count("\n")}
-    text1 = list(fulltext)
-    text2 = set(text1)
-    text3 = list(text2)
-    text3.sort()
-    text3.remove("\n")
-
-    thisnumberlist = []
-    for x in text3:
-        thisnumberlist.append(fulltext.count(x))
-
-    text4 = ''.join([c for c in fulltext if c not in ('!', '?', '.', ',', '/', '\\', '>', '<', '|', ';', ':', '{', '}',
-                                                      '[',']', '=', '+', '-', '(', ')', '*', '^', '%', '$', '`', '~',
-                                                      "'", '"')])
-    text4 = text4.replace("\n", " ")
-    text4 = text4.replace("\t", " ")
-    text4 = text4.split(" ")
-    text5 = set(text4)
-    text6 = list(text5)
-    text6.sort()
-    text6.remove("")
-
-    thisnumberlist2 = []
-    for x in text6:
-        thisnumberlist2.append(text4.count(x))
-
-    text_stat = {"digit_stat": digit_count(text4), "line_stat": linecount, "words_stat": word_count(text6,
-                 thisnumberlist2), "chars_stat": char_count(text3, thisnumberlist)}
-
-    return text_stat
-
+def circle(r):
+    if r < 0:
+        r *= -1
+    return 3.14159 * r**2
 
 if __name__ == '__main__':
+    print("Калькулятор площади фигур.\n"
+      "Введите 1 для Прямаугольника; 2 для Триугольника ; 3 для Круга ; 4 для выхода")
 
-    print("Программа подсчёта статистики теста!\nВведиет текс!:")
-    
-    text = ""
-    stopword = ""
-    while True:
-        line = input()
-        if line.strip() == stopword:
+while True:
+    try:
+        choice = int(input("Укажите номер действия: "))
+        if choice == 1:
+            x, y = [float(x) for x in input("Укажите 2 стороны прямоугольника через пробел: ").split()]
+            print("Площадь вашего прямоугольника равна: ", rect(x, y))
+            continue
+        elif choice == 2:
+            x, y, z = [float(x) for x in input("Укажите 3 стороны триугольника через пробел: ").split()]
+            print("Площадь вашего триугольника равна: ", triangle(x, y, z))
+            continue
+        elif choice == 3:
+            r = float(input("Укажите радиус круга: "))
+            print("Площадь вашего круга рана: ", circle(r))
+            continue
+        elif choice == 4:
+            print("Окончание программы!")
             break
-        text += "%s\n" % line
-    
-    arg = input(str("АРГУМЕНТ ЧЁЁ ПОПУТАЛ?!"))
-    digit = text_stat(text, arg).get("digit_stat")
-    line_ = text_stat(text, arg).get("line_stat")
-    char = text_stat(text, arg).get("chars_stat")
-    word = text_stat(text, arg).get("words_stat")
-    print("="*40, "\n")
-    print(''.join(['%s = %s \n' % (key, value) for (key, value) in digit.items()]))
-    print(''.join(['%s = %s \n' % (key, value) for (key, value) in line_.items()]))
-    print("="*40)
-    print("===== Начало = Словаря = Символов =====")
-    print(''.join(['%r = %s \n' % (key, value) for (key, value) in char.items()]))
-    print("====== Конец = Словаря = Символов ======")
-    print("="*40,)
-    print("======= Начало = Словаря = Слов =======")
-    print(''.join(['%r = %s \n' % (key, value) for (key, value) in word.items()]))
-    print("======== Конец = Словаря = Слов ========")
+        else:
+            print("Вы указали неверную команду, попробуйте еще раз!")
+            continue
+    except ValueError:
+        print("Вы ввели некоректное значение, попробуйте еще раз!")
+# Программа может принимать отрицательные значения, это предусмотрена как ошибка ввода пользователя(Защита от дурака. Длина не может быть отрицательной).
